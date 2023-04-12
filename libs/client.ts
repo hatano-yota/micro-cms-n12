952dae2c0babc8a7529005b7b09feb887c601e0d
unreact-hatano <mizutama.hahaha@gmail.com>
2023-04-12 20:50:08 +0900

    Type error解消

--- libs/client.ts
+++ libs/client.ts
@@ -1,6 +1,7 @@
 import { createClient } from "microcms-js-sdk";
 
+const apiKey = process.env.API_KEY ?? "default_api_key";
 export const client = createClient({
   serviceDomain: "hatanooo",
-  apiKey: process.env.API_KEY,
+  apiKey: apiKey,
 });
